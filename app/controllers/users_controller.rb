b4a7f79524b2e6256cd9a0da8395194d33168880
roberto daza <serviciosrobertodaza@gmail.com>
2021-10-09 10:26:25 -0500

    Se realizo la accion de actualizar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,40 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
+    unless @user
+      flash[:error] = "No se ha encontrado el usuario"
+      redirect_to root_path
+    end
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+    if @user
+      if @user.update(user_params)
+        flash[:notice] = "El usuario #{@user.name} fue actualizado"
+      else
+        flash[:error] = "El usuario no se pudo actualizar"  
+      end
+    else
+      flash[:error] = "El usuario no existe"
+    end
+    redirect_to root_path      
   end
 
   def destroy
+    @user = User.find_by(id: params[:id])
+    if @user
+      if @user.delete
+        flash[:notice] = "El usuario #{@user.name} fue eliminado"
+      else
+        flash[:error] = "El usuario #{@user.name} no se pudo eliminar"
+      end
+      redirect_to root_path
+    else
+      flash[:error] = "El usuario no existe"
+      redirect_to root_path
+    end
   end
 
   #strong_params
